52772563e31d9015f4c95007d089eea5cd2ebd45
Doktorlich <sergei199522@mail.ru>
2025-05-28 16:34:16 +0300

    Add pagination

--- src/controllers/post/read-post.ts
+++ src/controllers/post/read-post.ts
@@ -1,12 +1,28 @@
 import { NextFunction, Request, Response } from "express";
 import PostSchema from "../../models/post";
-import { RequestParams } from "../../types/post.types";
+import { RequestParams, RequestQuery } from "../../types/post.types";
 
 async function getPosts(req: Request, res: Response, next: NextFunction) {
-    const posts = await PostSchema.find();
+    const query = req.query as RequestQuery;
+    const params = req.params as RequestParams;
+    const POST_PER_PAGE: number = 2;
+    const page: number = +query.page || 1;
+
+    const cd: number = await PostSchema.find().countDocuments();
+    const posts = await PostSchema.find()
+        .skip((page - 1) * POST_PER_PAGE)
+        .limit(POST_PER_PAGE);
+
     res.status(200).render("post/index", {
         path: "/post",
         posts: posts,
+        cdPost: cd,
+        currentPage: page,
+        hasNextPage: POST_PER_PAGE * page < cd,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(cd / POST_PER_PAGE),
     });
 }
 
@@ -14,6 +30,7 @@ async function getPost(req: Request, res: Response, next: NextFunction) {
     const params = req.params as RequestParams;
     const postId = params.postId;
     const post = await PostSchema.findById(postId);
+
     res.status(200).render("post/full-post", {
         path: "/post/" + postId,
         paramId: postId,
--- src/types/post.types.ts
+++ src/types/post.types.ts
@@ -1,2 +1,3 @@
 export type RequestBody = { _id: string; text: string; title: string; content: string; creator: string };
 export type RequestParams = { postId: string };
+export type RequestQuery = { page: string };
